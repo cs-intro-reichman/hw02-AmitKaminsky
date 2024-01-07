6d5b34d9e75fa70984de714de45acfe4d24f375e
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-07 22:52:32 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -4,6 +4,13 @@
 public class Divisors {
 	public static void main (String[] args) {
 		int userInt = Integer.parseInt(args[0]);
+		/*
+  			#feedback:
+     			You didn't had to cover cases for negative numbers.
+			However, this implemantation is not right for negative numbers, since for any negative x = n * m
+			you should add whole these divisors: n, m, -n, -m since (-n)*(-m) = n * m = x
+   			In addition, you could do it with one loop without seperating these cases - but it might be more complex.
+		*/
 		if (userInt > 0) {
 			for(int i = 1; i <= userInt; i++) {
 				if (userInt % i == 0) {
